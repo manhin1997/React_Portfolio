0bf29ae519986026fce56b7f33e69be2da4c549f
Henry Lee <leemanhin1997@gmail.com>
2021-07-02 16:05:17 +0800

    Add back email href

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -7,7 +7,7 @@ export default function Menu({menuOpen,setmenuOpen, hoverOnTop}) {
                     <li><a href="#intro" onClick={()=> {setmenuOpen(false)}}>Intro</a></li>
                     <li><a href="#portfolio" onClick={()=> {setmenuOpen(false)}}>Portfolio</a></li>
                     <li><a href="#work" onClick={()=> {setmenuOpen(false)}}>Work</a></li>
-                    {/* <li><a href="#contact" onClick={()=> {setmenuOpen(false)}}>Contact</a></li> */}
+                    <li><a href="#contact" onClick={()=> {setmenuOpen(false)}}>Contact</a></li>
                 </ul>
         </div>
     )
